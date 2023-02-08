7940e8e1e0edd21e961e713e47cd3aaf816af781
CelsonF <celsondev@outlook.com>
2023-02-07 23:53:37 -0300

    Add bcrypt in server node

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
+const app = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
@@ -50,4 +52,4 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 
-app.listen(process.env.PORT || 3000, () => {});
+app.listen(process.env.PORT || 3000, () => { });
